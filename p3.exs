1eece815ccb9571624d470150df53c22d20480d3
Edwin Balani <git@balani.xyz>
2019-12-05 03:48:50 +0000

    Complete Puzzle 3 part 2

--- p3.exs
+++ p3.exs
@@ -1,5 +1,8 @@
 defmodule Puzzle do
-  def run(filename) do
+  def run1(filename), do: common(filename, &manhattan/2)
+  def run2(filename), do: common(filename, &pathlen/2)
+
+  defp common(filename, heuristic) do
     runs =
       File.read!(filename)
       |> String.split("\n")
@@ -9,18 +12,32 @@ defmodule Puzzle do
       runs
       |> Enum.map(&parse_line/1)
       |> Enum.map(&mark_run/1)
-      |> Enum.map(&MapSet.new/1)
+
+    # for run <- runs, point <- run do
+    #   IO.puts("#{elem(point, 0)}\t#{elem(point, 1)}")
+    # end
+
+    runsets = Enum.map(runs, &MapSet.new/1)
 
     hits =
-      apply(MapSet, :intersection, runs)
+      apply(MapSet, :intersection, runsets)
       |> Enum.filter(fn pos -> pos != {0, 0} end)
 
-    closest = Enum.min_by(hits, &manhattan/1)
-    IO.puts(manhattan(closest))
+    closest = Enum.min_by(hits, fn hit -> heuristic.(runs, hit) end)
+    IO.puts(heuristic.(runs, closest))
   end
 
-  defp manhattan({x, y}) do
+  defp manhattan(_runs, {x, y}) do
     abs(x) + abs(y)
+  end
+
+  defp pathlen(runs, point) do
+    steps =
+      runs
+      |> Enum.map(fn run -> Enum.find_index(run, fn p -> p == point end) end)
+      |> Enum.sum()
+
+    steps + 2
   end
 
   defp parse_line(line) do
@@ -49,34 +66,38 @@ defmodule Puzzle do
       :U ->
         %{
           turtle
-          | visited: turtle.visited ++ Enum.map(0..len, fn i -> {turtle.x, turtle.y + i} end),
+          | visited: turtle.visited ++ Enum.map(1..len, fn i -> {turtle.x, turtle.y + i} end),
             y: turtle.y + len
         }
 
       :D ->
         %{
           turtle
-          | visited: turtle.visited ++ Enum.map(0..len, fn i -> {turtle.x, turtle.y - i} end),
+          | visited: turtle.visited ++ Enum.map(1..len, fn i -> {turtle.x, turtle.y - i} end),
             y: turtle.y - len
         }
 
       :L ->
         %{
           turtle
-          | visited: turtle.visited ++ Enum.map(0..len, fn i -> {turtle.x - i, turtle.y} end),
+          | visited: turtle.visited ++ Enum.map(1..len, fn i -> {turtle.x - i, turtle.y} end),
             x: turtle.x - len
         }
 
       :R ->
         %{
           turtle
-          | visited: turtle.visited ++ Enum.map(0..len, fn i -> {turtle.x + i, turtle.y} end),
+          | visited: turtle.visited ++ Enum.map(1..len, fn i -> {turtle.x + i, turtle.y} end),
             x: turtle.x + len
         }
     end
   end
 end
 
-Puzzle.run("input/3-test1.txt")
-Puzzle.run("input/3-test2.txt")
-Puzzle.run("input/3.txt")
+Puzzle.run1("input/3-test1.txt")
+Puzzle.run1("input/3-test2.txt")
+Puzzle.run1("input/3.txt")
+IO.puts("")
+Puzzle.run2("input/3-test1.txt")
+Puzzle.run2("input/3-test2.txt")
+Puzzle.run2("input/3.txt")
